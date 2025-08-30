8a512575e03fdd863bdf5ef36d00164b62351309
Stephen Power <860644+StephenPower@users.noreply.github.com>
2025-08-30 18:40:59 -0400

    initial page updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Compounding Help";
-export const SITE_DESCRIPTION = "GLP-1 Compounding Help ";
+export const SITE_TITLE = "Online Beauty Bar";
+export const SITE_DESCRIPTION = "Find your wholesale supplies online";
